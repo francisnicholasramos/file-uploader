8097eaaf51071dc2d5ec3caca02bc0074aefa3db
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-12-10 17:52:50 +0800

    Add targeted directory expansion

---
+++ src/js/expandTree.js
@@ -0,0 +1,31 @@
+function expandDirectory() {
+    const path = window.location.pathname;
+    const id = path.split("/storage/").pop();
+
+    if (!id || id === '') return;
+
+    const targetDirectory = document.getElementById(`${id}`);
+
+    if (targetDirectory) {
+        targetDirectory.open = true;
+
+        // expand directories recursively
+        expandParentDirectories(targetDirectory)
+    }
+}
+
+function expandParentDirectories(params) {
+    let parent = params.closest('.subtree')
+
+    while (parent) {
+        const parentElement = parent.closest('details')
+
+        if (parentElement) {
+            parentElement.open = true;
+        }
+
+        parent = parentElement?.closest('.subtree')
+    }
+}
+
+document.addEventListener('DOMContentLoaded', expandDirectory)
