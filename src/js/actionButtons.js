6527324c53fb04c7ab33024818a51ad3b0f2df58
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-12-19 23:44:07 +0800

    add download file

---
+++ src/js/actionButtons.js
@@ -0,0 +1,25 @@
+const data = document.getElementById("entity-data")
+const entities = JSON.parse(data.textContent);
+
+const entityMap = entities.reduce((map, entity) => {
+  map[entity.id] = entity;
+  return map;
+}, {});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const download = document.querySelectorAll('.download')
+
+    download.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const modal = btn.closest('.modal')
+            const fileId = modal ? modal.dataset.id : btn.dataset.id
+            const file = entityMap[fileId]
+
+            if (file) {
+                window.location.href = `/storage/download/${fileId}`
+            }
+        })
+    })
+})
+
+// reduce(prev, currentVal)
