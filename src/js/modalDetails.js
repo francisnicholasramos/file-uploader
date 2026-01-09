1dc554fe17b337ff438256af5ecee41c9572aa06
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-01-09 10:24:43 +0800

    add pop-up dialog trigger

---
+++ src/js/modalDetails.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const file = document.querySelectorAll(".file")
+
+    // open modal
+    file.forEach(button => {
+        button.addEventListener("click", () => {
+            const id = button.dataset.id;
+
+            const targetModal = document.querySelector(
+                `dialog[data-id="${id}"]`
+            );
+
+            targetModal.showModal()
+        });
+    });
+
+    // close modal
+    document.querySelectorAll("[data-close-modal]").forEach(el => {
+        el.addEventListener("click", () => {
+            el.closest('dialog').close()  
+        })
+    })
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    const newFile = document.getElementById("file")
+    const newFolder = document.getElementById("folder")
+
+    if (newFile) {
+      newFile.addEventListener("click", () => {
+        document.getElementById("new-file").showModal()
+      })
+    }
+
+    if (newFolder) {
+      newFolder.addEventListener("click", () => {
+        document.getElementById("new-folder").showModal()
+      })
+    }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    const shareFolder = document.getElementById("share");
+    if (shareFolder) {
+        shareFolder.addEventListener("click", () => {
+            document.getElementById("share-folder").showModal();
+        });
+    }
+})
