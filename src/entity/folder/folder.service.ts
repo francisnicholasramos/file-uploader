d73ae8dc546a00cd62a2e4d167214d59c2de5e65
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-01-20 12:12:11 +0800

    add utilities for fetching folder data

--- src/entity/folder/folder.service.ts
+++ src/entity/folder/folder.service.ts
@@ -1,12 +1,66 @@
 import createError from "http-errors";
-import {getUserEntities} from "../entities.repository";
+import {
+    getUserEntities,
+    getDirEntityById,
+    getDirectoryTree,
+    getBreadCrumbs,
+    getDirectoryContents,
+} from "../entities.repository";
 
 export const getRootFolderData = async (userId: number) => {
-  const data = await getUserEntities(userId)
+    const entities = await getUserEntities(userId)
 
-  if (!data) throw new createError.NotFound();
+    if (!entities) throw new createError.NotFound();
 
-  return { entities: data };
+    const pathSegments = await getBreadCrumbs()
+
+    const directories = await getDirectoryTree(userId, null)
+
+    return { 
+        entities, 
+        pathSegments, 
+        directories 
+    };
 }
 
+export const getFolderEntities = async (
+    folderId: number,
+    userId: number,
+) => {
+    const dir = await getDirEntityById(folderId) 
+
+    if (!dir) throw new createError.NotFound()
+
+    const {childEntities, parentId} = dir
+    const directories = await getDirectoryTree(userId, null)
+    const pathSegments = await getBreadCrumbs(folderId)
+
+    return {
+        entities: childEntities,
+        pathSegments,
+        directories,
+        parentId
+    }
+
+}
+
+export const getPublicDirectoryData = async (
+    userId: number,
+    folderId: number,
+    rootFolderId: number
+) => {
+    const entities = await getDirectoryContents(folderId)
+
+    if (!entities) throw new createError.NotFound()
+
+    const pathSegments = await getBreadCrumbs(folderId)
+
+    const directories = await getDirectoryTree(userId, rootFolderId)
+
+    return {
+        entities,
+        pathSegments,
+        directories
+    }
+}
 
