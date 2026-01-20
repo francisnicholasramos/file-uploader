1ddaef619ed824c30f1d2112d83f16669c355bdf
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-01-20 12:26:35 +0800

    add generate share url functionality for public folders

--- src/js/generatePublicUrl.js
+++ src/js/generatePublicUrl.js
@@ -1,8 +1,9 @@
 function getShareFileUrl() {
-    document.querySelectorAll('select').forEach(select => {
+    // target only what's belong to "modal" scope
+    document.querySelectorAll('.modal select').forEach(select => {
         select.addEventListener('change', async (e) => {
             e.preventDefault();
-            const modal = e.target.closest('[data-modal]')
+            const modal = e.target.closest('.modal')
             const expiresIn = e.target.value
             const fileId = modal.dataset.id
             const generatedLink = modal.querySelector('.copyLink')
@@ -28,18 +29,48 @@ function getShareFileUrl() {
     })
 }
 
-// TODO: dataset from button with js href link produced by api
 function getShareDirectoryUrl() {
-    const shareDirectory = document.querySelector(".shareDirBtn")
-    const expiresIn = 1;
+    document.querySelectorAll('#share-folder select').forEach(select => {
+        const modal = select.closest("#share-folder");
+        const inputLink = modal.querySelector(".copyLink")
+        const copyBtn = modal.querySelector(".btn-primary");
 
-    shareDirectory.addEventListener("click", async () => {
-        if (dataset.id) {
-            const response = await fetch(`/share/folder/${folderId}}`)
-            const data = await response.json()
-        }
+        inputLink.style.display = 'none'
+        copyBtn.style.display = 'none'
+
+        select.addEventListener('change', async (e) => {
+            const path = window.location.pathname;
+            const folderId = path.split("/storage/").pop();
+            const expiresIn = e.target.value
+
+            if (folderId) {
+                try {
+                    inputLink.value = 'Generating URL...'
+                    inputLink.disabled =  true
+                    const res = await fetch(`/share/folder/${folderId}?expiresIn=${expiresIn}`)
+                    const data = await res.json()
+
+                    if (res.ok && data.publicDirectoryUrl) {
+                        inputLink.style.display = 'block'
+                        inputLink.value = data.publicDirectoryUrl;
+                        inputLink.disabled = false
+                        select.disabled = true
+                        copyBtn.style.display = 'block'
+
+                        copyBtn.addEventListener("click", () => {
+                            navigator.clipboard.writeText(data.publicDirectoryUrl)
+                            copyBtn.textContent = "Copied!"
+                        })
+                    }
+                } catch (error) {
+                    inputLink.value = 'Failed to generate URL.'
+                    inputLink.disabled = true
+                }
+            }
+
+        })
     })
 }
 
 document.addEventListener('DOMContentLoaded', getShareFileUrl)
-// document.addEventListener('DOMContentLoaded', getShareDirectoryUrl)
+document.addEventListener('DOMContentLoaded', getShareDirectoryUrl)
