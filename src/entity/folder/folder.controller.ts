65e78a63035ecdadf04e4f3a869bc6e5b024ad4f
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-01-20 12:06:48 +0800

    add create directory

--- src/entity/folder/folder.controller.ts
+++ src/entity/folder/folder.controller.ts
@@ -1,25 +1,64 @@
 import {Request, Response, NextFunction} from "express";
+import {createDirectory} from "../../entity/entities.repository";
 import createError from "http-errors";
-import {getRootFolderData} from "./folder.service";
+import {
+    getRootFolderData,
+    getFolderEntities
+} from "./folder.service";
 
 export const getFolder = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  try {
-    if (!req.user?.id) throw new createError.Unauthorized();
+    try {
+        if (!req.user?.id) throw new createError.Unauthorized();
 
-    const { id } = req.user
+        const { id, username } = req.user
 
-    const entities = await getRootFolderData(id)
+        const folderId = Number(req.params.folderId)
 
-    res.render("index", {
-        ...entities
-    })
+        console.log(folderId)
+        
+        const rootDirectory = {id: null, name: username}
 
-  } catch (error) {
-    next(error);
-  }
+        const entities = !folderId 
+            ? await getRootFolderData(id)
+            : await getFolderEntities(folderId, id)
+
+        res.render("index", {
+            folderId,
+            ...entities,
+            rootDirectory,
+            baseUrl: '/storage'
+        })
+
+    } catch (error) {
+        next(error);
+    }
 };
+
+export const handleCreateDirectory = async (
+    req:Request,
+    res:Response,
+    next:NextFunction
+) => {
+    try {
+        const userId = req.user?.id;
+
+        if (!userId) throw new createError.Unauthorized();
+
+        const directory = req.body.dir || 'Untitled folder'
+
+        const parentId = Number(req.body.parentId) || null
+
+        await createDirectory(directory, userId, parentId);
+
+        res.redirect(`/storage/${parentId || ''}`)
+
+    } catch (error) {
+        next(error) 
+    }
+
+}
 
