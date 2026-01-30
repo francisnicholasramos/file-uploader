69a8d9051fee0dc3fdd92ca2e6f474bb2bb4068a
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-01-30 16:52:51 +0800

    format route middlewares on separate lines for readability

--- src/public/public.router.ts
+++ src/public/public.router.ts
@@ -1,11 +1,17 @@
 import {Router} from "express";
 import {validateSharedDirectory} from "../middleware/validateSharedDirectory";
 import {
-    publicFileUrl,
+    publicFileDownload,
     publicDirectoryUrl,
 } from "./public.controller";
 
 const router = Router();
+
+router.get(
+    "/:sharedDirectoryId/download/:fileId",
+    validateSharedDirectory,
+    publicFileDownload
+)
 
 router.get(
     ['/:sharedDirectoryId', '/:sharedDirectoryId/:folderId'],
